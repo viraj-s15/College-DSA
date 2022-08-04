8cac3c4f399ad0e42e0268c0262830b98100da24
Viraj Shah <veer15102003@gmail.com>
2022-08-04 21:05:53 +0530

    Followed pep guidelines

--- linear_search.py
+++ linear_search.py
@@ -1,4 +1,4 @@
-def takeInput(array):
+def take_input(array):
 	#this function takes input from the user and stores it in the array
     nums = int(input("Enter the number of elements in the array: "))
     for i in range(nums):
@@ -11,25 +11,25 @@ def display(array):
     print(array)
 
 
-def searchElement(array):
+def search_element(array):
 	#this function is responsible for the linear sort mechanism
-    toBeSearched = int(input("Enter the element to be found: "))
-    indexOfElement = 0
+    to_be_searched = int(input("Enter the element to be found: "))
+    index_of_element = 0
     isFound = False
     for i in range(len(array)):
-        if toBeSearched == array[i]:
-            indexOfElement = i
+        if to_be_searched == array[i]:
+            index_of_element = i
             isFound = True
             break
 
     if isFound:
-        print("The element has been found at index", indexOfElement)
+        print("The element has been found at index", index_of_element)
     else:
         print("The element is not present in the array")
 
 
 if __name__ == "__main__":
     array = []
-    takeInput(array)
+    take_input(array)
     display(array)
-    searchElement(array)
+    search_element(array)
