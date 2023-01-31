633d01ee68ce0b95d9d7d0614e6c863eb196fdf6
viraj-s15 <veer15102003@gmail.com>
2023-01-31 15:39:14 +0530

    Added file:String matching -1

---
+++ Dynamic Programming/NaiveStringMatching.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+template<typename T>
+void printVector(vector<T> v) {
+    for (auto const& i : v) {
+        cout << i << " ";
+    }
+}
+
+void naiveStringMatching(string str1,string str2) { 
+    int l1 = str1.length();
+    int l2 = str2.length(); 
+    int j;
+    for (int i = 0; i <= l1-l2; i++) { 
+      for (j = 0; j < l2; j++) {
+        if (str1[i+j] != str2[j]) break; 
+      }
+      if (j == l2) cout<<"Pattern found at index "<<i<<endl; 
+    } 
+}
+ 
+int main() {
+    string str1,str2;
+    getline(cin,str1);
+    cin;
+    cin >> str2;
+    naiveStringMatching(str1,str2);
+    return 0;
+}
